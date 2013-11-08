b80d08cb20180f37a33ee86450537e549ace24be
Jared Roesch <roeschinc@gmail.com>
2013-11-08 12:30:18 -0800

    add initial code sketch

--- paxos-blog/src/Main.hs
+++ paxos-blog/src/Main.hs
@@ -1,5 +1,34 @@
 module Main where
 
+import Data.Functor ((<$>))
+import Data.IORef
+import Control.Monad
+import Control.Concurrent.Chan
+import qualified Data.Map as M
+import Control.Monad.State
+
+type ReplicaState = Boolean
+type Replica a = StateT ReplicaState IO a
+
+newValueRef :: (Value v) => IO (IORef (Just v))
+newValueRef = newIORef Nothing
+
 main :: IO ()
-main = putStrLn "Hello World!"
+main = do
+  ballotNum <- newIORef (0, 0)
+  acceptNum <- newIORef (0, 0)
+  acceptVal <- newValueRef
+  channels <- replicateM newChan
+  let peers = M.fromList $ zip [1..] channels
+  
+prepare :: Integer -> Chan a -> IO ()
+prepare c = undefined
+
+class Value a where
+  repr :: a = undefined
+
+promise :: (Value a) => Integer -> Integer -> a
+  
+
+leaderWait leader
 
