1b76acc88c8ca14e44e09b1401f67185b2b68360
Jared Roesch <roeschinc@gmail.com>
2013-11-11 14:37:23 -0800

    begin refactor of Message into seperate file

--- paxos-blog/src/Paxos/Directory.hs
+++ paxos-blog/src/Paxos/Directory.hs
@@ -1,24 +1,39 @@
 {-# LANGUAGE TupleSections #-}
 module Paxos.Directory where
 
+import qualified Control.Concurrent.Chan as C
 import Data.Functor ((<$>))
-import Control.Concurrent.Chan
 import qualified Data.Map as M
--- import Paxos.BiChan
+import Paxos.Message (Message)
 
 type Pid = Int
 type Message = String 
 
-data Process = Process Pid (Chan Message)
+data Process = Process Pid (C.Chan Message) deriving (Show, Eq)
 
 type Directory = M.Map Pid Process
 
---mkDirectory :: [Pid] -> IO Directory
---mkDirectory ps = sequence $ zipWith (fmap (1,)) $ map mkProcess ps
-  --where mkProcess pid = (Process pid) <$> newChan
+mkDirectory :: [Pid] -> IO Directory
+mkDirectory ps = do
+    pairs <- sequence $ zipWith (\p -> fmap (p,)) ps $ map mkProcess ps
+    return $ M.fromList pairs
+  where mkProcess pid = (Process pid) <$> C.newChan
         
-lookup :: Pid -> Process
-lookup = undefined
+plookup :: Directory -> Pid -> Process
+plookup dir pid = case pid `M.lookup` dir of
+  Nothing -> error "NO PID #YOLO"
+  Just v  -> v
+
+send :: Directory -> Pid -> Message -> IO ()
+send dir pid msg = 
+  let (Process _ chan) = plookup dir pid in
+    C.writeChan chan msg
+
+receive :: Process -> IO Message
+receive (Process _ chan) = C.readChan chan
+
+
+
 
 
 
